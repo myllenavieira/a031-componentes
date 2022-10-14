9bf18b06c1794a11990687cf339a8c108f5b405c
myllenavieira <vieirasilvamyllena@gmail.com>
2022-10-14 18:53:46 -0300

    Exercícios a031

---
+++ src/CardVideo.js
@@ -0,0 +1,55 @@
+import React from "react"
+import InfosUsuario from "./InfosUsuario";
+
+export default function CardVideo() {
+
+    const titulo = "Título do vídeo";
+
+    function reproduzVideo() {
+        alert("O vídeo está sendo reproduzido");
+    }
+    return (
+        <>
+            <div className="box-pagina-principal" onClick={reproduzVideo}>
+                <img src="https://picsum.photos/400/400?a=1 " alt="" />
+                <h4>{titulo}</h4>
+                <InfosUsuario/>
+            </div>
+            <div className="box-pagina-principal" onClick={reproduzVideo}>
+                <img src="https://picsum.photos/400/400?a=2 " alt="" />
+                <h4>{titulo}</h4>
+                <InfosUsuario/>
+            </div>
+            <div className="box-pagina-principal" onClick={reproduzVideo}>
+                <img src="https://picsum.photos/400/400?a=3 " alt="" />
+                <h4>{titulo}</h4>
+                <InfosUsuario/>
+            </div>
+            <div className="box-pagina-principal" onClick={reproduzVideo}>
+                <img src="https://picsum.photos/400/400?a=4 " alt="" />
+                <h4>{titulo}</h4>
+                <InfosUsuario/>
+            </div>
+            <div className="box-pagina-principal" onClick={reproduzVideo}>
+                <img src="https://picsum.photos/400/400?a=5 " alt="" />
+                <h4>{titulo}</h4>
+                <InfosUsuario/>
+            </div>
+            <div className="box-pagina-principal" onClick={reproduzVideo}>
+                <img src="https://picsum.photos/400/400?a=6 " alt="" />
+                <h4>{titulo}</h4>
+                <InfosUsuario/>
+            </div>
+            <div className="box-pagina-principal" onClick={reproduzVideo}>
+                <img src="https://picsum.photos/400/400?a=7 " alt="" />
+                <h4>{titulo}</h4>
+                <InfosUsuario/>
+            </div>
+            <div className="box-pagina-principal" onClick={reproduzVideo}>
+                <img src="https://picsum.photos/400/400?a=8 " alt="" />
+                <h4>{titulo}</h4>
+                <InfosUsuario/>
+            </div>
+        </>
+    )
+}
---
+++ src/InfosUsuario.js
@@ -0,0 +1,11 @@
+import React from "react";
+import user from "./img/user.svg"
+
+export default function InfosUsuario(){
+    return (
+    <>
+    <img src={user} alt="" className="user"/>
+    <p>Nome do usuário: Usuário 1</p>
+    </>
+    )
+}
